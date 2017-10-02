e746795ca5b6aa262e3b2fe9520de86b4a3fdd44
fabian.mesaglio@aseguratuviaje.com <fabian.mesaglio@aseguratuvieja.com>
2017-10-02 12:55:53 -0300

    modificaton to test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 var app = require('../app.js');
- 
+ //this is a test
 describe('GET /', function() {
   it('respond with hello world', function(done) {
     request(app).get('/').expect('hello jenkins', done);
